438b23233bbd7f75b5481ef5059bdec4dd0def80
ayushkabachaanik-cyber <ayushkabachaanik@gmail.com>
2026-06-05 07:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 app.use(express.json());
 
 const IS_VERCEL = !!process.env.VERCEL || !!process.env.NOW_REGION;
